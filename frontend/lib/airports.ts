34b47ca6dd13ddd3e7c9a0f91c3f3e72e1532893
Lewis Wearmouth <lewistaku@gmail.com>
2025-11-15 15:17:05 -0500

    south america trips

--- frontend/lib/airports.ts
+++ frontend/lib/airports.ts
@@ -2,37 +2,46 @@ export const AIRPORTS: Record<
     string,
     { coords: [number, number]; name: string }
 > = {
-    LHR: { coords: [-0.4543, 51.4700], name: "London Heathrow Airport" },
-    BCN: { coords: [2.0785, 41.2974], name: "Barcelona-El Prat Airport" },
+
+    // NORTH AMERICA ---------------------------
     IAD: { coords: [-77.4565, 38.9531], name: "Washington Dulles International Airport" },
-    EDI: { coords: [-3.3615, 55.9508], name: "Edinburgh Airport" },
+    ORD: { coords: [-87.9048, 41.9742], name: "Chicago O'Hare International Airport" },
+    BOS: { coords: [-71.0052, 42.3656], name: "Boston Logan International Airport" },
+    PHL: { coords: [-75.2411, 39.8729], name: "Philadelphia International Airport" },
+    SEA: { coords: [-122.3088, 47.4502], name: "Seattle-Tacoma International Airport" },
+    BNA: { coords: [-86.6782, 36.1263], name: "Nashville International Airport" },
+    SDF: { coords: [-85.7360, 38.1744], name: "Louisville Muhammad Ali International Airport" },
+    SJU: { coords: [-66.0018, 18.4394], name: "Luis Muñoz Marín International Airport (San Juan)" },
+    IND: { coords: [-86.2944, 39.7173], name: "Indianapolis International Airport" },
+
+    // SOUTH AMERICA ---------------------------
     EZE: { coords: [-58.5358, -34.8222], name: "Buenos Aires Ministro Pistarini (Ezeiza) Airport" },
     SCL: { coords: [-70.7858, -33.3930], name: "Santiago International Airport (SCL)" },
     LIM: { coords: [-77.1143, -12.0219], name: "Lima Jorge Chávez International Airport" },
     MVD: { coords: [-56.0263, -34.8311], name: "Montevideo Carrasco International Airport" },
-    SJU: { coords: [-66.0018, 18.4394], name: "Luis Muñoz Marín International Airport (San Juan)" },
+    AQP: { coords: [-71.3440, -16.3411], name: "Arequipa Rodríguez Ballón International Airport" },
+    CUZ: { coords: [-71.9410, -13.5357], name: "Cusco Alejandro Velasco Astete International Airport" },
+
+    // EUROPE ---------------------------------
+    LHR: { coords: [-0.4543, 51.4700], name: "London Heathrow Airport" },
+    BCN: { coords: [2.0785, 41.2974], name: "Barcelona-El Prat Airport" },
+    EDI: { coords: [-3.3615, 55.9508], name: "Edinburgh Airport" },
     CDG: { coords: [2.5670, 49.0097], name: "Paris Charles de Gaulle Airport" },
     ZAG: { coords: [16.0615, 45.7429], name: "Zagreb Airport" },
     VCE: { coords: [12.3519, 45.5053], name: "Venice Marco Polo Airport" },
     BZO: { coords: [11.3264, 46.4603], name: "Bolzano Airport" },
-    HAN: { coords: [105.8042, 21.2212], name: "Hanoi Noi Bai International Airport" },
-    ORD: { coords: [-87.9048, 41.9742], name: "Chicago O'Hare International Airport" },
-    BKK: { coords: [100.7501, 13.6900], name: "Bangkok Suvarnabhumi Airport" },
-    BOS: { coords: [-71.0052, 42.3656], name: "Boston Logan International Airport" },
-    PHL: { coords: [-75.2411, 39.8729], name: "Philadelphia International Airport" },
     LJU: { coords: [14.4576, 46.2237], name: "Ljubljana Jože Pučnik Airport" },
-    SEA: { coords: [-122.3088, 47.4502], name: "Seattle-Tacoma International Airport" },
     NCE: { coords: [7.2159, 43.6584], name: "Nice Côte d'Azur Airport (for Monaco)" },
-    BNA: { coords: [-86.6782, 36.1263], name: "Nashville International Airport" },
-    AQP: { coords: [-71.3440, -16.3411], name: "Arequipa Rodríguez Ballón International Airport" },
-    SDF: { coords: [-85.7360, 38.1744], name: "Louisville Muhammad Ali International Airport" },
-    PNH: { coords: [104.8440, 11.5466], name: "Phnom Penh International Airport" },
-    MRS: { coords: [5.2214, 43.4393], name: "Marseille Provence Airport" },
-    SGN: { coords: [106.6620, 10.8185], name: "Ho Chi Minh City Tan Son Nhat Airport" },
-    CUZ: { coords: [-71.9410, -13.5357], name: "Cusco Alejandro Velasco Astete International Airport" },
-    JLP: { coords: [7.1250, 43.5357], name: "Cannes–Mandelieu Airport (for Antibes)" },
-    DAD: { coords: [108.1990, 16.0439], name: "Da Nang International Airport (for Hoi An)" },
+    JLP: { coords: [7.1250, 43.5357], name: "Cannes-Mandelieu Airport (for Antibes)" },
     ADX: { coords: [-2.8684, 56.3729], name: "RAF Leuchars / St Andrews" },
+    MRS: { coords: [5.2214, 43.4393], name: "Marseille Provence Airport" },
+
+    // SOUTHEAST ASIA --------------------------
+    HAN: { coords: [105.8042, 21.2212], name: "Hanoi Noi Bai International Airport" },
+    BKK: { coords: [100.7501, 13.6900], name: "Bangkok Suvarnabhumi Airport" },
+    SGN: { coords: [106.6620, 10.8185], name: "Ho Chi Minh City Tan Son Nhat Airport" },
     REP: { coords: [103.8130, 13.4120], name: "Siem Reap Angkor International Airport" },
-    IND: { coords: [-86.2944, 39.7173], name: "Indianapolis International Airport" },
+    PNH: { coords: [104.8440, 11.5466], name: "Phnom Penh International Airport" },
+    DAD: { coords: [108.1990, 16.0439], name: "Da Nang International Airport (for Hoi An)" },
+
 };
--- frontend/lib/flights.ts
+++ frontend/lib/flights.ts
@@ -1,11 +1,24 @@
 // lib/flights.ts
 
 export const TRIPS = [
-    { origin: "TPA", destination: "HND" },
-    { origin: "TPA", destination: "LHR" },
-    { origin: "HND", destination: "TPA" },
-    { origin: "IAD", destination: "LHR" },
-    { origin: "LHR", destination: "BCN" },
+    { origin: "IAD", destination: "BCN" }, // DC to Barcelona
+    { origin: "IAD", destination: "EDI" }, // DC to Edinburgh
+
+    // South America
+    // December 2024/January 2025 New Years
+    { origin: "IAD", destination: "EZE" }, // DC to Buenos Aires
+    { origin: "EZE", destination: "SCL" }, // Buenos Aires to Santiago
+    { origin: "EZE", destination: "MVD" }, // Buenos Aires to Montevideo 
+    { origin: "SCL", destination: "IAD" }, // Santiago to DC
+
+    // May 2025 Machu Picchu!
+    { origin: "SDF", destination: "LIM" }, // Louisville to Lima
+    { origin: "LIM", destination: "CUZ" }, // Lima to Cusco
+    { origin: "CUZ", destination: "AQP" }, // Cusco to Arequipa
+    { origin: "AQP", destination: "LIM" }, // Arequipa to Lima
+
+    // Puerto Ricoooo
+    { origin: "IND", destination: "SJU" }, // Indianapolis to San Juan
 
     // Add your real trips here!
     // Example:
